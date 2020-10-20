fa9506bac915f82d83beed393428ac7e4446aed2
dat-lzt <2726219176@qq.com>
2020-10-20 09:12:20 +0800

    移除dist

--- mytest/src/components/Rule/rule.jsx
+++ mytest/src/components/Rule/rule.jsx
@@ -4,7 +4,7 @@ import styles from './rule.scss';
 export default function rule(props) {
     return (
         <div className={styles.Rule} style={{ visibility: props.AlertShow ? 'visible' : 'hidden' }}>
-          <div className={`${styles.ruleconnent} ${styles.ruleConnentActive}`}>
+          <div className={props.AlertShow ?`${styles.ruleconnent} ${styles.ruleConnentActive}`:`${styles.ruleconnent}`}>
             <div className={styles.ruletitle}>活动说明</div>
             <img className={styles.allRules} src={require("../../assets/allRules.png")} alt="" />
             <img onClick={props.onRuleclose} className={styles.ruleclose} src={require("../../assets/ruleclose.png")} alt="" />
